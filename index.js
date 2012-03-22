134945f6748bfb04fea540f1810f7b7817b59920
Forbes Lindesay <forbes@lindesay.co.uk>
2012-03-22 22:49:06 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+//ip should usually begin 239, port can be any number.
+
+/**
+ * Sends an recieves multicast JSON messages asyncronously
+ * @param {function(message, address, port)}    onMessage   A function to call when a message is recieved
+ * @param {object}  [options]                   Optional object containing options
+ * @param {string}  [options.ip]                A string containing the IP address of the multicast group, which should match 239.0.0.0/8
+ * @param {int}     [options.port]              A port number for the multicast group, must be the same for everyone.
+ * @param {string}  [options.socketType]        The type of socket to use (defaults to "udp4")
+ * @return {function(message)}  Send message function
+ */
+module.exports = function multicast(onMessage, options) {
+    var dgram = require("dgram");
+    options = options || {};
+    var port = options.port || 41834;
+    var ip = options.ip || "239.0.0.73";
+    
+    var server = dgram.createSocket(options.socketType||"udp4");
+
+    server.on("listening", function () {
+        var address = server.address();
+        console.log("server listening " + address.address + ":" + address.port);
+    });
+
+    server.bind(port);
+    server.addMembership(ip);
+
+
+    server.on("message", function (msg, rinfo) {
+        onMessage(JSON.parse("" + msg), rinfo.address, rinfo.port);
+    });
+
+    return function sendMessage(m) {
+        var message = new Buffer(JSON.stringify(m));
+            server.send(message, 0, message.length, port, ip);
+    };
+};
---
+++ readme.md
@@ -0,0 +1,31 @@
+A server which sends and recieves multicast JSON messages to a single IP address and port number using UDP.
+
+#Example Usage
+
+```js
+var send = require("multicast")(function(message, address, port){
+    console.log(address + ":" + port + " says " + JSON.stringify(message));
+});
+send("hello world");
+send({complexObject:[1,3,5]});
+```
+
+# Create Server (onMessage, options)
+
+Multicast is a function to create a server.  It takes a callback to recieve messages, and options.
+
+## Parameters
+
+### onMessage(Message, Address, Port)
+
+Called with the message as an object, the IP address the message is from as a string, and the port number as an int.
+
+### Options
+
+ - ip: set the IP address for multicasting, which should match 239.0.0.0/8 (i.e. the ip address should consist of 4 bytes separted by '.' and the first byte should be 239)
+ - port: The port number for multicasting, any port number can be used, providing it's not already in use for something else.  The same port number should be used by all devices wishing to communicate.
+ - socketType: defaults to udp4, you can use this to move to udp6 once ipv6 has widespread support (note that you'll also need to change the IP address.
+
+## Returns Send(Message)
+
+The function returned by creating the server is the send method.  Simply pass it any object which can be serialised to JSON to have it sent as a message, be aware that you'll recieve all your own messages back too.
